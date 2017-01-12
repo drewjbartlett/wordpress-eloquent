8ba9dd8b9792c123e814fad0f528f02742d20350
Drew Bartlett <drewb9@gmail.com>
2017-01-11 20:10:25 -0800

    updated gitignore

--- Test/TestPost.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace Test;
-
-    class TestPost extends \WPEloquent\Model\Post {
-        public function trails() {
-            return $this->terms()->where('taxonomy', 'trail');
-        }
-    }
- ?>
