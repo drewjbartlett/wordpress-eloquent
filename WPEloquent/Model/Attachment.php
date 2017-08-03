552494c6279f993415f6a5d245b70b817a344bf2
Drew Bartlett <drewb9@gmail.com>
2017-08-03 09:26:23 -0700

    formatting fix and version tag

--- WPEloquent/Model/Attachment.php
+++ WPEloquent/Model/Attachment.php
@@ -3,7 +3,7 @@
 namespace WPEloquent\Model;
 
 class Attachment extends Post {
-	public function post() {
+	public function post () {
 		return $this->belongsTo(Post::class, 'post_parent', 'ID');
 	}
 }
