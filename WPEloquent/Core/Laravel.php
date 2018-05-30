da66879c97963a6269885f7f3aa857f94306475e
Blaise <blaise.dufrain@saltermitchell.com>
2018-05-30 14:00:52 -0400

    master: multiple connections

--- WPEloquent/Core/Laravel.php
+++ WPEloquent/Core/Laravel.php
@@ -3,75 +3,138 @@ namespace WPEloquent\Core;
 
 use Config;
 
-class Laravel {
+class Laravel
+{
 
     protected static $_capsule;
 
     /**
      * [capsule description]
-     * @param  array  $options [description]
+     *
+     * @param  array $options [description]
      * @return [type]          [description]
      * @author drewjbartlett
      */
-    public static function connect ($options = []) {
+    public static function connect($options = [])
+    {
 
-        $defaults = [
-            'global' => true,
-
-            'config' => [
-                'database' => [
-                    'user'     => '',
-                    'password' => '',
-                    'name'     => '',
-                    'host'     => '',
-                    'port'     => '3306'
-                ],
-
-                'prefix' => 'wp_'
-            ],
-
-            'events' => false,
-
-            'log'    => true
-        ];
-
-        $options = array_replace_recursive($defaults, $options);
-
-        if(is_null(self::$_capsule)) {
+        if (is_null(self::$_capsule)) {
 
             self::$_capsule = new \Illuminate\Database\Capsule\Manager();
-
-            self::$_capsule->addConnection([
-                'driver'    => 'mysql',
-                'host'      => $options['config']['database']['host'],
-                'database'  => $options['config']['database']['name'],
-                'username'  => $options['config']['database']['user'],
-                'password'  => $options['config']['database']['password'],
-                'port'      => $options['config']['database']['port'],
-                'charset'   => 'utf8',
-                'collation' => 'utf8_unicode_ci',
-                'prefix'    => $options['config']['prefix']
-            ]);
+            if (is_array($options['config']['database'])) {
+                self::addMultipleConnections($options);
+            } else {
+                self::addSingleConnection($options);
+            }
 
             self::$_capsule->bootEloquent();
 
-            if($options['events']) self::$_capsule->setEventDispatcher(new \Illuminate\Events\Dispatcher);
+            if ($options['events']) {
+                self::$_capsule->setEventDispatcher(new \Illuminate\Events\Dispatcher);
+            }
 
-            if($options['global']) self::$_capsule->setAsGlobal();
+            if ($options['global']) {
+                self::$_capsule->setAsGlobal();
+            }
 
-            if($options['log']) self::$_capsule->getConnection()->enableQueryLog();
+            if ($options['log']) {
+                self::$_capsule->getConnection()->enableQueryLog();
+            }
 
         }
 
         return self::$_capsule;
     }
 
-    public static function getConnection () {
-        return self::$_capsule->getConnection();
+    public static function getConnection($name = null)
+    {
+        return self::$_capsule->getConnection($name);
     }
 
-    public static function queryLog () {
+    public static function queryLog()
+    {
         return self::getConnection()->getQueryLog();
     }
+
+    /**
+     * @param mixed $capsule
+     */
+    public static function addSingleConnection($options)
+    {
+        $defaults = [
+            'global' => true,
+            'config' => [
+                'database' => [
+                    'user'     => '',
+                    'password' => '',
+                    'name'     => '',
+                    'host'     => '',
+                    'port'     => '3306',
+                ],
+                'prefix' => 'wp_',
+            ],
+            'events' => false,
+            'log' => true,
+        ];
+        $options = array_replace_recursive($defaults, $options);
+        self::$_capsule->addConnection([
+            'driver'    => 'mysql',
+            'host'      => $options['config']['database']['host'],
+            'database'  => $options['config']['database']['name'],
+            'username'  => $options['config']['database']['user'],
+            'password'  => $options['config']['database']['password'],
+            'port'      => $options['config']['database']['port'],
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => $options['config']['prefix'],
+        ], $name);
+    }
+
+    /**
+     * @param mixed $capsule
+     */
+    public static function addMultipleConnections($options)
+    {
+        $defaults = [
+            'global' => true,
+
+            'config' => [
+                'database' => [
+                    'default' => [
+                        'user'     => '',
+                        'password' => '',
+                        'name'     => '',
+                        'host'     => '',
+                        'port'     => '3306',
+                    ]
+                ],
+
+                'prefix' => '',
+            ],
+
+            'events' => false,
+
+            'log' => true,
+        ];
+        $options = array_replace_recursive($defaults, $options);
+
+        foreach ($options['config']['database'] as $name => $config) {
+            if (count($options['config']['database']) === 1) {
+                $name = 'default';
+            }
+            self::$_capsule->addConnection([
+                'driver'    => 'mysql',
+                'host'      => $config['host'],
+                'database'  => $config['name'],
+                'username'  => $config['user'],
+                'password'  => $config['password'],
+                'port'      => $config['port'],
+                'charset'   => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix'    => isset($config['prefix']) ? $config['prefix'] : null,
+            ], $name);
+        }
+    }
 }
+
 ?>
